afbf0f438dd574dd3faef6d5d01e04fb4dd9ec02
root <root@localhost.localdomain>
2023-04-18 13:47:06 +0800

    feat: 新增搜索等功能

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,16 +5,28 @@ export default defineConfig({
   lang: 'zh-CN',
   title: '微软电脑管家文档',
   description: '微软电脑管家使用及问题帮助文档',
-
   lastUpdated: true,
   cleanUrls: 'without-subfolders',
   base: '/',
 
   themeConfig: {
+    nav: [{
+      text: '官方网站',
+      link: 'https://pcmanager.microsoft.com'
+    },
+    {
+      text: '关注我们',
+      items: [
+        { text: '官方微博', link: 'https://weibo.com/mspcmanager' },
+        { text: '官方公众号', link: 'http://weixin.qq.com/r/VyhzazfExmq5rXMI931F' },
+        { text: '官方 QQ 群', link: 'https://forms.office.com/pages/responsepage.aspx?id=v4j5cvGGr0GRqy180BHbR7Jc7yMooGtIh8-T51_PVotUNkZSWVJSQVo4SFZVUUtDSDVHRjA4MTFMWi4u' },
+        { text: '官方 Discord', link: 'https://discord.com/invite/FTDTddsPgd' }
+      ]
+    }],
 
     editLink: {
       pattern: 'https://github.com/mcpcmanager/mspcm-docs/edit/main/:path',
-      text: '在 github 上编辑此页'
+      text: '在 Github 上编辑此页'
     },
 
     socialLinks: [
@@ -24,6 +36,10 @@ export default defineConfig({
     footer: {
       message: '本文档基于 Apache 2.0 协议发布',
       copyright: 'Copyright © 2023 Microsoft PC Manager Administrators'
+    },
+
+    search: {
+      provider: 'local'
     }
   }
 })
